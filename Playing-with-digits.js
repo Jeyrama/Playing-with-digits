57ff4019bc1bedd460cd08d66b17aa6ff99f069f
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-09-30 11:02:12 -0700

    Update Playing-with-digits.js

--- Playing-with-digits.js
+++ Playing-with-digits.js
@@ -30,3 +30,10 @@ function digPow(n, p) {
 }
 
 // or
+
+function digPow(n, p) {
+  let ans = (''+n).split('')
+    .map(function(d,i){return Math.pow(+d,i+p)})
+    .reduce(function(s,v){return s+v}) / n
+  return ans%1 ? -1 : ans
+}
